18092a1d2096a64b9c9b440c61c54c50dd1451ef
diplg <dimas4354@icloud.com>
2022-05-25 22:43:51 +0300

    задание 1

---
+++ task1/Program.cs
@@ -0,0 +1,37 @@
+﻿// Задайте двумерный массив. Напишите программу, которая поменяет местами первую и последнюю строку массива.
+
+Console.Clear();
+Console.Write("Введите число строк: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите чисто стобцов: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int[,] mass = new int[m, n];
+for (int i = 0; i < mass.GetLength(0); i++)
+{
+    for (int j = 0; j < mass.GetLength(1); j++)
+    {
+        mass[i, j] = new Random().Next(1, 9);
+        Console.Write(mass[i, j] + " ");
+    }
+    Console.WriteLine();
+}
+Console.WriteLine();
+int save;
+for (int i = 0; i < mass.GetLength(1); i++)
+{
+    //Console.Write(mass[0, i] + " ");
+    save = mass[0,i];
+    //Console.Write(mass[mass.GetLength(0)-1,i]+" ");
+    mass[0,i] = mass[mass.GetLength(0)-1,i];
+    mass[mass.GetLength(0)-1,i] = save;
+}
+
+for (int i = 0; i < mass.GetLength(0); i++)
+{
+    for (int j = 0; j < mass.GetLength(1); j++)
+    {
+        Console.Write(mass[i, j] + " ");
+    }
+    Console.WriteLine();
+}
+Console.WriteLine();
